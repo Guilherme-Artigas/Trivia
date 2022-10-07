993dc499da183065a7d8f33b46e309fe81bba2ac
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-10-07 16:54:29 -0300

    Requisito 03

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Config from './Pages/Config';
 import Login from './Pages/Login';
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <div>
       <Switch>
         <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+        <Route path="/config" render={ (props) => <Config { ...props } /> } />
       </Switch>
     </div>
   );
