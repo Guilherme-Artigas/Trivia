93ec31e6396f5a6b26e88ad8bff77f6a22625fc9
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-10-08 12:34:16 -0300

    Requisito 06 não finalizado.2

--- src/Pages/Trivia.jsx
+++ src/Pages/Trivia.jsx
@@ -1,62 +1,107 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { func, shape } from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { getTokenLocal } from '../utils/localStorage';
 import { getQuestions } from '../utils/requestApi';
+
+const INVALID_TOKEN = 3;
 
 class Trivia extends Component {
   state = {
     token: '',
     code: 0,
-    results: [],
+    currentQuestion: {},
+    idQuestion: 0,
+    scrambledObject: {},
   };
 
   componentDidMount() {
     this.setState({ token: getTokenLocal() }, async () => {
-      const { token } = this.state;
+      const { token, idQuestion } = this.state;
       const responseAPI = await getQuestions(token);
+      const currentQuestion = responseAPI.results[idQuestion];
       this.setState({
         code: responseAPI.response_code,
-        results: responseAPI.results,
+        currentQuestion,
       }, () => {
-        const { code } = this.state;
-        const { history: { push } } = this.props;
-        const invalidToken = 3;
-        if (code === invalidToken) {
-          localStorage.removeItem('token');
-          push('/');
-        }
+        this.shufflesAnswers();
       });
     });
   }
 
+  shufflesAnswers = () => {
+    const { currentQuestion: question } = this.state;
+    const questionObject = {
+      category: question.category,
+      difficulty: question.difficulty,
+      question: question.question,
+      type: question.type,
+      options: [{
+        id: 0,
+        value: question.correct_answer,
+        isCorrect: true,
+      },
+      ...question.incorrect_answers.map((option, index) => ({
+        id: index + 1,
+        value: option,
+        isCorrect: false,
+      }))] };
+    const maximum = 4;
+    const minimum = 2;
+    const size = questionObject.type === 'multiple' ? maximum : minimum;
+    const newList = [];
+    while (newList.length < size) {
+      const randomNumber = Math.floor(Math.random() * size);
+      if (!newList.includes(questionObject.options[randomNumber])) {
+        newList.push(questionObject.options[randomNumber]);
+      }
+    }
+    const scrambledObject = { ...questionObject, options: newList };
+    return this.setState({ scrambledObject });
+  };
+
   render() {
-    const { results } = this.state;
+    const { scrambledObject, code } = this.state;
+
     return (
       <>
         <h1>Game</h1>
         <section>
-          {results.length > 0 && (
+          {scrambledObject.options !== undefined && (
             <>
-              <p data-testid="question-category">{`category: ${results[0].category}`}</p>
-              <p data-testid="question-text">{`question: ${results[0].question}`}</p>
+              <p data-testid="question-category">
+                {`category: ${scrambledObject.category}`}
+              </p>
+              <p data-testid="question-text">
+                {`question: ${scrambledObject.question}`}
+              </p>
+              {console.log(scrambledObject)}
               <div data-testid="answer-options">
-                <button type="button" data-testid="correct-answer">
-                  {results[0].correct_answer}
-                </button>
-                {results[0].incorrect_answers.map((e, i) => (
-                  <button
-                    key={ `${e}-${i}` }
-                    data-testid={ `wrong-answer-${i}` }
-                    type="button"
-                  >
-                    {e}
-                  </button>
+                {scrambledObject.options.map(({ value, isCorrect }, i) => (
+                  isCorrect ? (
+                    <button
+                      type="button"
+                      key={ `answer-${i}` }
+                      data-testid="correct-answer"
+                    >
+                      {value}
+                    </button>
+                  ) : (
+                    <button
+                      type="button"
+                      key={ `answer-${i}` }
+                      data-testid={ `wrong-answer-${i}` }
+                    >
+                      {value}
+                    </button>
+                  )
                 ))}
               </div>
             </>
           )}
         </section>
+        {code === INVALID_TOKEN && <Redirect to="/" />}
       </>
     );
   }
@@ -78,6 +123,9 @@ export default connect()(Trivia);
 // Será validado se as alternativas estão presentes
 // Será validado se a quantidade de alternativas corretas é 1
 // Será validado se as alternativas estão posicionadas em ordem aleatória
+
+// data-testid="correct-answer"
+// data-testid={ `wrong-answer-${i}` }
 
 // {
 //   "category": "Geography",
