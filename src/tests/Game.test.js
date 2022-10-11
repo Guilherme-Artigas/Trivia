32186d05eb1477c6001b1521ccfbe55ab2198b26
marcelsguima <marcelsguima@gmail.com>
2022-10-11 20:36:32 -0300

    Requisito 21

---
+++ src/tests/Game.test.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+import App from '../App';
+
+describe('Testes se na tela Game:',()=>{
+
+beforeEach(async () =>{
+    const { history } = renderWithRouterAndRedux(<App />);
+
+    const name = screen.getByTestId("input-player-name");
+    const email = screen.getByTestId("input-gravatar-email");
+
+    userEvent.type(email, "trybe@teste.com");
+    expect(email).toHaveValue("trybe@teste.com")
+    userEvent.type(name, "trybe");
+    expect(name).toHaveValue("trybe")
+
+    const buttonPlay = screen.getByTestId("btn-play");
+    
+    userEvent.click(buttonPlay);
+    await waitFor(() => expect(history.location.pathname).toBe('/game'))
+})
+
+it('a pergunta aparece corretamente.', async ()=>{
+
+    const question = await screen.findByTestId('question-text');
+    expect(question).toBeInTheDocument()
+  
+    const correctAnswer = screen.getByTestId("correct-answer");
+    expect(correctAnswer).toBeInTheDocument();
+
+    userEvent.click(correctAnswer);
+    expect(correctAnswer.className).toBe('correct-answer')
+
+    const wrongAnswer = screen.getAllByTestId(/wrong-answer-/i);
+    wrongAnswer.forEach((element) => {
+        expect(element).toBeInTheDocument();
+        expect(element.className).toBe('incorrect-answer')
+    })
+   
+    const nextBtn = screen.getByRole('button', {  name: /next/i});
+    expect(nextBtn).toBeInTheDocument();
+  
+  });
+
+  
+  jest.setTimeout(40000);
+  it('se o cronômetro funciona corretamente', async () => {
+
+    const nextBtn = await screen.findByRole('button', {  name: /next/i}, {timeout:35000});
+    expect(nextBtn).toBeInTheDocument();
+
+    userEvent.click(nextBtn);
+
+    const stopWatch = screen.getByRole('heading', {  name: /30 segundos restantes\./i});
+    expect(stopWatch).toBeInTheDocument();
+
+})
+
+})
